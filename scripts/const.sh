8d218c139097304794acfad74c290fb097908a2b
djagydms <mnbbang2@naver.com>
2021-07-22 00:05:21 +0900

    feature: change to dev run

--- scripts/const.sh
+++ scripts/const.sh
@@ -7,11 +7,22 @@ bdir=$shdir/build
 # fabric-version
 fv=2.2.2
 
+# for prod
+# ORDERER_ADDR=orderer0.pusan.ac.kr:7050
+# GLOBAL_FLAGS="-o $ORDERER_ADDR --tls --cafile /etc/hyperledger/fabric/orderer-tls/tlsca.pusan.ac.kr-cert.pem"
+# ORGANIZATIONS=(management verification-01 verification-02 trader)
+# PEERS=(peer0.management.pusan.ac.kr peer0.verification-01.pusan.ac.kr peer0.verification-02.pusan.ac.kr peer0.trader.pusan.ac.kr)
+# CHANNELS=(data trade ai-model)
+# CHAINCODE_DIR=/etc/hyperledger/fabric/chaincodes
+# CHAINCODES=(data trade ai-model)
+# VERSION=1.0
+
+# for dev
 ORDERER_ADDR=orderer0.pusan.ac.kr:7050
 GLOBAL_FLAGS="-o $ORDERER_ADDR --tls --cafile /etc/hyperledger/fabric/orderer-tls/tlsca.pusan.ac.kr-cert.pem"
 ORGANIZATIONS=(management verification-01 verification-02 trader)
 PEERS=(peer0.management.pusan.ac.kr peer0.verification-01.pusan.ac.kr peer0.verification-02.pusan.ac.kr peer0.trader.pusan.ac.kr)
-CHANNELS=(data trade ai-model)
+CHANNELS=(trade)
 CHAINCODE_DIR=/etc/hyperledger/fabric/chaincodes
-CHAINCODES=(data trade ai-model)
+CHAINCODES=(trade)
 VERSION=1.0
